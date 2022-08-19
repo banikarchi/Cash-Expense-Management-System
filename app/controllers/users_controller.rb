282ff5252d2404acc2a1f116a28f0a00b4e3b6d4
Archisman Banik <archisman.banik@kreeti.com>
2022-08-20 03:15:53 +0530

    C2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,11 @@ class UsersController < ApplicationController
 
 
   end
+  def destroy
+    @user=User.find(params[:id])
+    @user.destroy
+    
+  end
   private 
   def user_params
     params.require(:user).permit(:email, :password, :is_admin)
