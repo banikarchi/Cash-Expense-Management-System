9d32b2a8f2aef0df65a111e8f62b7394d761a827
Archisman Banik <archisman.banik@kreeti.com>
2022-08-21 06:43:24 +0530

    change

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     @user=User.new
   end
   def create
-    @user=User.find_by(email: user_params[:email])
+    @user = User.find_by(email: user_params[:email])
     if @user && @user.is_password?(user_params[:password])
       session[:user_id]=@user.id 
       
